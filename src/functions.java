471d916de4e95130ed04389ea29ee52b3faaebe8
deecee <deebandeeban6@gmail.com>
2023-03-18 14:07:47 +0530

    just adding two nums by calling a func

---
+++ src/functions.java
@@ -0,0 +1,11 @@
+public class functions {
+    public static void main(String[] args) {
+        int sum = answer(10 , 20);
+        System.out.println("The sum is " + sum);
+
+    }
+    static int answer(int a , int b){
+        int answer = a + b;
+        return answer;
+    }
+}
