16491245e654419444b7a58991db079f9e909db2
deecee <deebandeeban6@gmail.com>
2023-06-22 21:22:17 +0530

    Bubble Sort Algorithm && Insertion Sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,8 +1,11 @@
 import java.util.Arrays;
+
+
 public class BubbleSort {
     public static void main(String[] args){
-        int[] arr = {5,4,3,2,1};
-        bubblesort(arr);
+        int[] arr = {5,-4,3,-2,1,0};
+        //bubblesort(arr);
+        selectionsort(arr);
         System.out.println(Arrays.toString(arr));
     }
     public static void bubblesort(int[] arr) {
@@ -19,4 +22,30 @@ public class BubbleSort {
             if(swapped == true) break;
         }
     }
+
+    public static void selectionsort(int[] arr){
+        for(int i = 0; i< arr.length; i++){
+            int last = arr.length-1-i;
+            int maxIndex = getmaxIndex(arr, 0, last );
+            swap(arr, maxIndex, last);
+        }
+    }
+
+    private static void swap(int[] arr, int first , int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    private static int getmaxIndex(int[] arr, int start,int end) {
+        int max = start;
+        for(int i = start; i<=end; i++){
+            if(arr[i] > arr[max] ){
+                max = i;
+            }
+        }
+        return max;
+    }
+
+
 }
